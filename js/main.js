01603f4d1269eeb619e2fcb502e021cdb6a693f9
mit <mithelash.c@outlook.in>
2019-11-20 14:56:49 -0500

    Final Commit
    Change-Id: Id9c6935086e5479754cc97600362fb5f742fa403

--- js/main.js
+++ js/main.js
@@ -1,31 +1,3 @@
-/*$(window).on("load resize scroll",function(){
-    $("#header_name").addClass("invisible");
-    //For Identifying the top and height
-    var fixed_header_height = $("#fixed-header").outerHeight();
-    var window_height = ($(window).height());
-    var body_container_height = window_height - fixed_header_height;
-    $('.body-container').css({ top: fixed_header_height +'px' });
-    $('.body-container').css({ height: body_container_height +'px' });
-    $('.rest').css({ top: fixed_header_height +'px' });
-    var name_container_height = ($("#name").outerHeight());
-    var name_container_position = $("#name-cont").position();
-    var scrollLimit = name_container_height + name_container_position.top;
-    //Toggle
-    if ($("#name").scrollTop > scrollLimit || document.documentElement.scrollTop > scrollLimit) {
-        $("#name-cont").addClass("invisible");
-        $("#header_name").removeClass("invisible");
-    } else {
-        $("#name-cont").removeClass("invisible");
-        $("#header_name").addClass("invisible");
-    }
-
-    if ($("#arrow").scrollTop > body_container_height || document.documentElement.scrollTop > body_container_height) {
-        $("#arrow").addClass("invisible");
-    } else {
-        $("#arrow").removeClass("invisible");
-    }
-});*/
-
 var ScrollPosition=0;
 var ScrollWindowHeight = 0;
 var docHeight = 0;
@@ -34,7 +6,6 @@ $(document).ready(function(){
     $(window).on("scroll",function(){
         ScrollMouse();
         headerPosition();
-        console.log(checkVisibility());
 
     });
     $(window).on("resize",function(){
@@ -49,10 +20,6 @@ $(document).ready(function(){
         jQuery(this).find('.skillbar-bar').animate({
             width: jQuery(this).attr('data-percent')
         }, 3000);
-    });
-    $(".sub-title .rotate").textrotator({
-        animation: "fade",
-        speed: 1000
     });
 });
 function setTitleContainerHeight(){
